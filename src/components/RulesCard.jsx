3b0aea16e1044f8b3e4f62859410b6e04e4e74de
Dimitris_Dr <jidrist18@hotmail.com>
2024-12-13 14:02:21 +0200

    updated font color in rules card and box shadow

--- src/components/RulesCard.jsx
+++ src/components/RulesCard.jsx
@@ -13,7 +13,7 @@ export default function RulesCard() {
     return (
         <dialog ref={dialogRef} className="rules-card fadeIn">
             <div className="dialog-content grid-item">
-                <h2 className="title rules-title fw-700">Rules</h2>
+                <h2 className="title rules-title fw-700 txt-dark">RULES</h2>
                 <div className="img-container close-img-container">
                     <img src="/rock-paper-scissors-react/images/image-rules-bonus.svg" alt="Rules image" className="img rules-img" />
                 </div>
